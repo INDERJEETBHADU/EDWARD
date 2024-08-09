2d1edebe63fe40e3fce9d2013d4466538256453e
vishalmanglaw453 <vishalmanglaw@gmail.com>
2024-08-09 12:54:51 +0530

    back_to_top_add

--- assets/app.js
+++ assets/app.js
@@ -59,3 +59,24 @@ video.forEach((e) => {
 function form(event) {
   event.preventDefault();
 }
+
+// =========== back to top ============ 
+
+let backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+backToTopBtn.onclick = function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+};
+
